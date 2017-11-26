5da35b11df1054bd3230bc019e1edd2c18b997a8
liyinuo0823 <you@example.compip>
2017-11-26 16:48:06 +0800

    add new file

---
+++ baidu.py
@@ -0,0 +1,9 @@
+import os
+
+from selenium import webdriver
+
+drvier=webdriver.Chrome(executable_path="./chromedriver")
+
+drvier.get ("https://www.baidu.com/")
+baiduserach=drvier.find_element_by_id("kw")
+baiduserach.send_keys("helloworld")
